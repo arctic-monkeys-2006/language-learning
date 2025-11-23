78ab069cd5d2325ab44d9900bea869c0a6c45f28
arctic-monkeys-2006 <arctic-monkeys-2006@jnstn.uk>
2025-11-23 17:57:36 +0000

    added spanish bit :)

--- learn-a-language/main.py
+++ learn-a-language/main.py
@@ -3,52 +3,63 @@ import random
 
 
 def main():
-    words = readwords()
     print("Hello from learn-a-language!")
     while True:
+        print("choose language: ")
+        language = input("g: German, s: Spanish: ")
+        if language == "g":
+            words = readwords("german")
+            language = "German"
+        elif language == "s":
+            words = readwords("spanish")
+            language = "Spanish"
+        else:
+            print("Invalid language selected.")
+            continue
+
         print("Choose mode: ")
         print("1: Revision Mode")
         print("2: Test English")
-        print("3: Test German")
+        print(f"3: Test {language}")
         print("q: Quit")
         mode = input("Enter your selection: ")
         if mode == "1":
-            revisionmode(words)
+            revisionmode(words, language)
         elif mode == "2":
-            testegnlish(words)
+            testegnlish(words, language)
         elif mode == "3":
-            testgerman(words)
+            testgerman(words, language)
         elif mode == "q":
             break
         else:
             print("Invalid mode selected.")
 
 
-def readwords():
+def readwords(language):
     words = []
-    with open("languages/german.csv") as f:
+    with open(f"languages/{language}.csv", encoding="utf-8") as f:
         reader = csv.reader(f)
         for row in reader:
             words.append(row)
     return words
 
 
-def revisionmode(words):
+def revisionmode(words, language):
     while True:
         index = random.randint(0, len(words) - 1)
         word = words[index]
-        print(f"German: '{word[0]}'")
+        print(f"{language}: '{word[0]}'")
         print(f"English: '{word[1]}'")
         quit = input("Press Enter to continue... or type 'q' to quit: ")
         if quit == "q":
             break
 
 
-def testegnlish(words):
+def testegnlish(words, language):
     while True:
         index = random.randint(0, len(words) - 1)
         word = words[index]
-        print(f"German: '{word[0]}'")
+        print(f"{language}: '{word[0]}'")
         guesse = input("type english ... or type 'q' to quit: ")
         if guesse == "q":
             break
@@ -59,12 +70,12 @@ def testegnlish(words):
             print(f"Wrong, the correct answer is '{english}'")
 
 
-def testgerman(words):
+def testgerman(words, language):
     while True:
         index = random.randint(0, len(words) - 1)
         word = words[index]
         print(f"english: '{word[1]}'")
-        guessg = input("type german ... or type 'q' to quit: ")
+        guessg = input(f"type {language} ... or type 'q' to quit: ")
         if guessg == "q":
             break
         german = word[0]
