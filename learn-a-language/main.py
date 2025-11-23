96bbf3937afd89df9b357d0c3e1a6d64becea925
arctic-monkeys-2006 <arctic-monkeys-2006@jnstn.uk>
2025-11-23 14:54:04 +0000

    started on it. did revision and test english :)

---
+++ learn-a-language/main.py
@@ -0,0 +1,60 @@
+import csv
+import random
+
+
+def main():
+    words = readwords()
+    print("Hello from learn-a-language!")
+    while True:
+        print("Choose mode: ")
+        print("1: Revision Mode")
+        print("2: Test English")
+        print("q: Quit")
+        mode = input("Enter 1 or 2: ")
+        if mode == "1":
+            revisionmode(words)
+        elif mode == "2":
+            testegnlish(words)
+        elif mode == "q":
+            break
+        else:
+            print("Invalid mode selected.")
+
+
+def readwords():
+    words = []
+    with open("languages/german.csv") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            words.append(row)
+    return words
+
+
+def revisionmode(words):
+    while True:
+        index = random.randint(0, len(words) - 1)
+        word = words[index]
+        print(f"German: '{word[0]}'")
+        print(f"English: '{word[1]}'")
+        quit = input("Press Enter to continue... or type 'q' to quit: ")
+        if quit == "q":
+            break
+
+
+def testegnlish(words):
+    while True:
+        index = random.randint(0, len(words) - 1)
+        word = words[index]
+        print(f"German: '{word[0]}'")
+        guesse = input("type english ... or type 'q' to quit: ")
+        if guesse == "q":
+            break
+        english = word[1]
+        if guesse == english:
+            print("Correct")
+        else:
+            print(f"Wrong, the correct answer is '{english}'")
+
+
+if __name__ == "__main__":
+    main()
